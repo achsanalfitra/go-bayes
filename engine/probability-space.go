4604fc376e043244a47f5bf62fe58cff1c185f28
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-07 23:45:21 +0700

    feat(basic): add validation flag on probability space, along with methods to check validity and total probability. Also inserted the total probability check to addpair so new probability input can be immediately against the object validity

--- engine/probability-space.go
+++ engine/probability-space.go
@@ -4,21 +4,48 @@ import "fmt"
 
 // ProbabilitySpace is defined as pairs of sample and its probability
 type ProbabilitySpace struct {
-	space map[string]float64
+	space              map[string]float64
+	isProbabilitySpace bool // Indicates whether the user current probability space is valid
 }
 
 // Create a new ProbabilitySpace
 func NewProbabilitySpace() *ProbabilitySpace {
 	return &ProbabilitySpace{
-		space: make(map[string]float64),
+		space:              make(map[string]float64),
+		isProbabilitySpace: true,
 	}
 }
 
 // Add a new event and probability pair
 func (ps *ProbabilitySpace) AddPair(event string, probability float64) {
+
+	// Input first-check
 	if probability < 0 || probability > 1 {
 		fmt.Println("Error: enter probability value between 0 and 1")
 		return
 	}
-	ps.space[event] = probability
+
+	// Check input against total probability
+	totalProb := ps.CheckProb() + probability
+
+	if totalProb > 1 {
+		fmt.Println("Warning: your input makes total probability exceeding 1")
+		ps.isProbabilitySpace = false
+	} else {
+		ps.space[event] = probability
+	}
+}
+
+// Check total probability
+func (ps *ProbabilitySpace) CheckProb() float64 {
+	totalProb := 0.0
+	for _, prob := range ps.space {
+		totalProb += prob
+	}
+	return totalProb
+}
+
+// Check probability probability space validity
+func (ps *ProbabilitySpace) IsValid() bool {
+	return ps.CheckProb() <= 1.0
 }
