686ce987feabdb15462ac459c9762af5fec3a823
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-24 18:59:40 +0700

    feat(probability context): migrate joint definition in probability context

--- egn/probabilitycontext.go
+++ egn/probabilitycontext.go
@@ -1,22 +1,18 @@
 package egn
 
 type ProbabilityContext struct {
-	Marginal           map[string]map[string]struct{}                       // format {nodeName: {event : }}
-	Conditional        map[string]map[string]map[string]struct{}            // format {nodeName: {dependencyCombination: {event: }}}
-	PartialConditional map[string]map[string]map[string]map[string]struct{} // format {fullJointFactors {partialFactors: {dependencyCombinations: {event: }}}}
-	Joint              map[string]map[string]struct{}                       // format {factors : {jointEvent: } }
-	PartialJoint       map[string]map[string]map[string]struct{}            // format {fullJointFactors: {partialJointFactors: {event: }}} (partials must have full joint)
-	NodeName           map[string]*Node                                     // format {nodeName: }
+	Marginal    map[string]map[string]struct{}            // format {nodeName: {event : }}
+	Conditional map[string]map[string]map[string]struct{} // format {nodeName: {dependencyCombination: {event: }}}
+	Joint       map[string]*ProbabilitySpace              // format {factors : {space: {eventspace: value}} }
+	NodeName    map[string]*Node                          // format {nodeName: }
 }
 
 func BuildContext() *ProbabilityContext {
 	return &ProbabilityContext{
-		Marginal:           make(map[string]map[string]struct{}),
-		Conditional:        make(map[string]map[string]map[string]struct{}),
-		PartialConditional: make(map[string]map[string]map[string]map[string]struct{}),
-		Joint:              make(map[string]map[string]struct{}),
-		PartialJoint:       make(map[string]map[string]map[string]struct{}),
-		NodeName:           make(map[string]*Node),
+		Marginal:    make(map[string]map[string]struct{}),
+		Conditional: make(map[string]map[string]map[string]struct{}),
+		Joint:       make(map[string]*ProbabilitySpace),
+		NodeName:    make(map[string]*Node),
 	}
 }
 
@@ -24,10 +20,4 @@ func (pc *ProbabilityContext) ShowMarginal() {
 	for nodeName := range pc.Marginal {
 		pc.NodeName[nodeName].Show.MarginalEvents()
 	}
-}
-
-func (pc *ProbabilityContext) CheckConsistency(nodeName string) {
-	// Check itself on joint ledger, if exists check marginal event existence on the ledger
-	// If exists check its marginal probability
-	//
 }
