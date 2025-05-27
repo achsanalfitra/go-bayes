ff0b47163da4c87fc90d436953bbc93bb1a93ccc
Alfitra Achsan <achsanalfitra@gmail.com>
2025-05-27 23:18:51 +0700

    feat(CPT): add create combinations function to encode int to radix enumeration

--- egn/cpt.go
+++ egn/cpt.go
@@ -1,5 +1,7 @@
 package egn
 
+import "fmt"
+
 type CPT struct {
 	Missing  map[string]struct{}
 	Existing map[string]struct{}
@@ -14,12 +16,31 @@ func CreateCPT() *CPT {
 
 func (n *Node) CombinationsIndex() []int {
 	// assign parents length as index
-	parentsLength := len(n.Parents)
-	combinationsIndex := make([]int, 0, parentsLength)
+	combinationsIndex := make([]int, 0, len(n.Parents))
 
 	for _, parent := range n.Parents {
 		combinationsIndex = append(combinationsIndex, len(parent.States))
 	}
 
 	return combinationsIndex
+}
+
+func (n *Node) CreateCombinations(input int) ([]int, error) {
+	// return error if out of range
+	if input > n.TotalStates()-1 {
+		return nil, fmt.Errorf("input out of range, the maximum is %d", n.TotalStates()-1)
+	}
+
+	combinationIndex := n.CombinationsIndex()
+	output := make([]int, 0, len(n.Parents))
+
+	// iterate from 0 to totalcombinations length - 1
+	for i := 0; i < len(combinationIndex); i++ {
+
+		// append output with modulo
+		output = append(output, input%combinationIndex[i])
+		input /= combinationIndex[i]
+	}
+
+	return output, nil
 }
