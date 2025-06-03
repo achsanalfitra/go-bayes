ee1403d8ff562fc591b5984fa3de4fae6ec5be3e
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-03 22:35:30 +0700

    helper(bi map int): implement addkey function with a minimal safe guarding mechanism

--- hlp/bidirectionalmap.go
+++ hlp/bidirectionalmap.go
@@ -1,5 +1,7 @@
 package hlp
 
+import "fmt"
+
 type BiMapInt struct {
 	StrInt map[string]int
 	IntStr map[int]string
@@ -10,4 +12,18 @@ func NewBiMapInt() *BiMapInt {
 		StrInt: make(map[string]int),
 		IntStr: make(map[int]string),
 	}
+}
+
+func (b *BiMapInt) AddKey(key string) error {
+	// safeguard against overwrite
+	if _, exist := b.StrInt[key]; exist {
+		return fmt.Errorf("key already exists")
+	}
+
+	// one source of truth for int id
+	id := len(b.StrInt)
+	b.StrInt[key] = id
+	b.IntStr[id] = key
+
+	return nil
 }
